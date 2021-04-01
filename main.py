f8ee1f261e152fa9395662866edfbbe186bf4c53
Bomb Moss <jordanlow.jinyi@gmail.com>
2021-04-01 08:49:33 +0000

    Halfway through bus stop search functionality

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from flask import *
+import csv, json
+
+routes, services, stops = [[],[],[]]
+
+with open('data/bus_routes.json') as f:
+    routes = json.load(f)
+
+with open('data/bus_services.json') as f:
+    services = json.load(f)
+
+with open('data/bus_stops.json') as f:
+    stops = json.load(f)
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.form.get('search', False) == 'YESPLEASE':
+        return render_template('search-busstop.html', stop=stop)
+    param = request.form.get('param', None)
+    return render_template('search.html', param=param)
 
 app.run('0.0.0.0')
